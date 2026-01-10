259b61654a945b2ace98a2ac35f48aec47664a2e
TMXQWQ <qq273761857@163.com>
2026-01-10 18:54:42 +0800

    rebuilding

--- kernel/kmain.c
+++ kernel/kmain.c
@@ -1,1 +1,22 @@
+#include <stdint.h>
 
+void executable_entry(void)
+{
+    const char msg[] = "Logically you should use Limine to boot it instead of executing it directly, right?\n\n";
+    __asm__ volatile("mov $1, %%rax\n"
+                     "mov $1, %%rdi\n"
+                     "lea %[msg], %%rsi\n"
+                     "mov %[len], %%rdx\n"
+                     "syscall\n"
+                     "mov $60, %%rax\n"
+                     "mov $1, %%rdi\n"
+                     "syscall\n"
+                     :
+                     : [msg] "m"(msg), [len] "r"(sizeof msg - 1)
+                     : "rax", "rdi", "rsi", "rdx");
+}
+
+void kernel_entry(void)
+{
+    for (;;);
+}
